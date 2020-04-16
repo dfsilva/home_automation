b70cf6d9a6394998d9afadf2f50267e5696bafc6
Diego Silva <diegosiuniube@gmail.com>
2020-04-16 09:12:43 -0300

    transm

--- spots/transmiter/transmiter.ino
+++ spots/transmiter/transmiter.ino
@@ -4,28 +4,25 @@
 #include "printf.h"
 
 RF24 radio(9,10);
+const uint64_t w_pipes[2] =  {0xABCDABCD71LL, 0x544d52687CLL};
 
-//Topology
-const uint64_t pipes[2] =  {0xABCDABCD71LL, 0x544d52687CLL};
+int last_pipe = 0;
 
 void setup() {
     Serial.begin(57600);
     printf_begin();
     radio.begin();
     radio.enableDynamicPayloads();
-
-    //radio.setDataRate(RF24_250KBPS);
-    //radio.setPALevel(RF24_PA_MAX);
     radio.setChannel(55);
-
     radio.setRetries(15,15);
-    //radio.setCRCLength(RF24_CRC_16);
 }
 
 void loop() {
-  int p_index = random(0, 1);
-  radio.openWritingPipe(pipes[p_index]);
+  Serial.println(last_pipe); 
+  radio.openWritingPipe(w_pipes[last_pipe]);
   transmitTemperature();
+  last_pipe = (last_pipe == 0) ? 1 : 0;
+  delay(500);
 }
 
 void transmitTemperature(){
@@ -33,7 +30,7 @@ void transmitTemperature(){
     char temp[10];
     dtostrf(t,6,2,temp);
     char msg[40];
-    int id = random(1, 2000);
+    int id = random(1, 10);
     sprintf(msg, "id:temp_%d,T:temp,value:%s",id,temp);
     Serial.println(msg); 
     bool ok = radio.write(&msg,strlen(msg));

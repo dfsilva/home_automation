74450d49f200ba88ad171cd8b7b7374386727cbc
Diego <diego@diegosilva.com.br>
2020-04-16 12:31:21 -0300

    fixes

--- server/src/main/java/br/com/diegosilva/automation/routes/AutomationRoutes.java
+++ server/src/main/java/br/com/diegosilva/automation/routes/AutomationRoutes.java
@@ -3,12 +3,18 @@ package br.com.diegosilva.automation.routes;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Adapter;
 import akka.cluster.sharding.typed.javadsl.ClusterSharding;
+import akka.cluster.sharding.typed.javadsl.EntityRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.Route;
 import akka.serialization.jackson.JacksonObjectMapperProvider;
+import br.com.diegosilva.automation.actors.Device;
+import br.com.diegosilva.automation.dto.IOTMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
 
@@ -34,10 +40,34 @@ public class AutomationRoutes {
                         get(() -> path("health", () -> {
 
                             return complete("Hello from " + system.address());
-                        }))
-
+                        })),
+                        pathPrefix("device", () ->
+                                concat(
+                                        post(() ->
+                                                entity(
+                                                        Jackson.unmarshaller(objectMapper, IOTMessage.class),
+                                                        data -> onConfirmationReply(sendMessage(data))))
+                                )
+                        )
                 )
         );
+    }
+
+
+    private Route onConfirmationReply(CompletionStage<Device.Response> reply) {
+        return onSuccess(reply, confirmation -> {
+            if (confirmation instanceof Device.SendResponse)
+                return complete(StatusCodes.OK, ((Device.SendResponse) confirmation).message, Jackson.marshaller(objectMapper));
+            else
+                return complete(StatusCodes.BAD_REQUEST, "Erro");
+        });
+    }
+
+
+    private CompletionStage<Device.Response> sendMessage(IOTMessage data) {
+        EntityRef<Device.Command> entityRef =
+                sharding.entityRefFor(Device.TypeKey, data.id);
+        return entityRef.ask(replyTo -> new Device.Send(data, replyTo), timeout);
     }
 
 

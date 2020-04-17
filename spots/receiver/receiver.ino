91ed14524ca2a00a1b3ab3321363229dcbfaf9a7
Diego <diego@diegosilva.com.br>
2020-04-17 07:37:19 -0300

    melhorias

--- spots/receiver/receiver.ino
+++ spots/receiver/receiver.ino
@@ -5,6 +5,9 @@
 
 RF24 radio(9,10);
 const uint64_t r_pipes[2] =  {0xABCDABCD71LL, 0x544d52687CLL};
+const uint64_t t_pipe = 0xABCDABCD71AA;
+
+const int IDX_PIPE = 0;
 
 void setup(){
     Serial.begin(57600);
@@ -17,15 +20,32 @@ void setup(){
     radio.setRetries(15,15);
     radio.setCRCLength(RF24_CRC_16);
 
-    radio.openReadingPipe(1,r_pipes[1]);  
+    radio.openWritingPipe(t_pipe);
+    radio.openReadingPipe(1,r_pipes[IDX_PIPE]);  
     radio.startListening();
 }
 
 void loop(){
-   if (radio.available()) {
+    readWifi();
+    readSerial();
+}
+
+void readWifi(){
+  if (radio.available()) {
         int len = radio.getDynamicPayloadSize();
         char msg[40] = "";
         radio.read(&msg,len);
         Serial.println(msg);  
-   } 
+   }
+}
+
+void readSerial(){
+  String serialMsg = Serial.readString();
+  if(serialMsg != ""){
+     Serial.println("Mensagem serial: "+serialMsg); 
+     radio.openWritingPipe(r_pipe);
+     char msg[20];
+     serialMsg.toCharArray(msg, strlen(msg));
+     radio.write(&msg,strlen(msg));
+  }
 }
--- spots/transmiter/transmiter.ino
+++ spots/transmiter/transmiter.ino
@@ -5,6 +5,7 @@
 
 RF24 radio(9,10);
 const uint64_t w_pipes[2] =  {0xABCDABCD71LL, 0x544d52687CLL};
+const uint64_t r_pipe = 0xABCDABCD71AA;
 
 int last_pipe = 0;
 
@@ -15,14 +16,29 @@ void setup() {
     radio.enableDynamicPayloads();
     radio.setChannel(55);
     radio.setRetries(15,15);
+
+    radio.setCRCLength(RF24_CRC_16);
+    radio.openReadingPipe(1,r_pipe);  
+    radio.startListening();
 }
 
 void loop() {
-  Serial.println(last_pipe); 
   radio.openWritingPipe(w_pipes[last_pipe]);
   transmitTemperature();
   last_pipe = (last_pipe == 0) ? 1 : 0;
+  readWifi();
   delay(200);
+  
+}
+
+void readWifi(){
+   if (radio.available()) {
+        int len = radio.getDynamicPayloadSize();
+        char msg[40] = "";
+        radio.read(&msg,len);
+        String msgStr = msg;
+        Serial.println("Mensagem wifi: "+msgStr);  
+   } 
 }
 
 void transmitTemperature(){
@@ -32,6 +48,6 @@ void transmitTemperature(){
     char msg[20];
     int id = random(1, 10);
     sprintf(msg, "id:temp_%d,T:temp,value:%s",id,temp);
-    Serial.println(msg); 
-    bool ok = radio.write(&msg,strlen(msg));
+//    Serial.println(msg); 
+    radio.write(&msg,strlen(msg));
 }

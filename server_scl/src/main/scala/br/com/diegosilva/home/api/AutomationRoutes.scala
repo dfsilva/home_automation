730fe0d14a2d60cdd084f0dbeabd2a3ef5e72e6d
Diego Silva <diegosiuniube@gmail.com>
2020-04-22 18:17:22 -0300

    ajustes

--- server_scl/src/main/scala/br/com/diegosilva/home/api/AutomationRoutes.scala
+++ server_scl/src/main/scala/br/com/diegosilva/home/api/AutomationRoutes.scala
@@ -1,11 +1,11 @@
 package br.com.diegosilva.home.api
 
-
 import akka.NotUsed
 import akka.actor.typed.ActorRef
 import akka.actor.typed.scaladsl.ActorContext
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Directives.{handleWebSocketMessages, path, _}
 import akka.http.scaladsl.server.PathMatchers.Remaining
 import akka.http.scaladsl.server.Route
 import akka.stream.OverflowStrategy
@@ -14,18 +14,28 @@ import akka.stream.typed.scaladsl.{ActorSink, ActorSource}
 import br.com.diegosilva.home.actors.UserWs
 import br.com.diegosilva.home.actors.UserWs.{Calculate, Calculated, Fail, WsHandleDropped}
 import io.circe.generic.auto._
-import io.circe.parser._
+import io.circe.parser.decode
 import io.circe.syntax._
 
 import scala.concurrent.duration._
 
+object AutomationRoutes {
 
-trait Routes {
+}
 
+class AutomationRoutes()(implicit context: ActorContext[_]) {
 
-  private def wsUser(userName: String, context: ActorContext[Any]): Flow[Message, Message, NotUsed] = {
+  private val sharding = ClusterSharding(context.system)
 
-    val wsUser: ActorRef[UserWs.Command] = context.spawn(UserWs(), s"user-$userName")
+  val routes: Route = {
+    path("ws" / Remaining) { userName: String =>
+      handleWebSocketMessages(wsUser(userName, context))
+    }
+  }
+
+  private def wsUser(userName: String, context: ActorContext[_]): Flow[Message, Message, NotUsed] = {
+
+    val wsUser: ActorRef[UserWs.Command] = context.spawn(new UserWs(userName).create(), s"user-$userName")
 
     val sink: Sink[Message, NotUsed] =
       Flow[Message].collect {
@@ -49,15 +59,10 @@ trait Routes {
           wsUser ! UserWs.ConnectWsHandle(wsHandler)
           NotUsed
         })
-        .keepAlive(maxIdle = 10.seconds, () => TextMessage.Strict("Keep-alive message sent to WebSocket recipient"))
+        .keepAlive(maxIdle = 10.seconds, () => TextMessage.Strict("Mensagem de Keep-alive enviada pelo recipent websocket"))
 
     Flow.fromSinkAndSource(sink, source)
   }
 
-  def routes(context: ActorContext[Any]): Route = {
-    path("ws" / Remaining) { userName: String =>
-      handleWebSocketMessages(wsUser(userName, context))
-    }
-  }
 
 }

155562f9ab968ddb6a1ac52f7dd3440b8e1f06c2
Diego <diegosiuniube@gmail.com>
2020-05-19 14:20:46 -0300

    ajustes

--- rf_network_jni/src/main/java/MainTest.java
+++ rf_network_jni/src/main/java/MainTest.java
@@ -3,6 +3,6 @@ import br.com.diegosilva.rfnative.RfNative;
 public class MainTest {
     public static void main(String ... args){
         System.out.println(System.getProperty("java.library.path"));
-        new RfNative().start();
+        new RfNative().start(00);
     }
 }

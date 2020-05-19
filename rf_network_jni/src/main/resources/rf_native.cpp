9905cc9922f8c4e0abd26c75e70e2b9c5dc22229
Diego <diegosiuniube@gmail.com>
2020-05-19 15:34:36 -0300

    ajustes

--- rf_network_jni/src/main/resources/rf_native.cpp
+++ rf_network_jni/src/main/resources/rf_native.cpp
@@ -11,37 +11,36 @@ RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
 
 RF24Network network(radio);
 
-JNIEXPORT void JNICALL Java_br_com_diegosilva_rfnative_RfNative_start
-  (JNIEnv *env, jobject thiz, jint node){
-    radio.begin();
-    radio.setDataRate(RF24_250KBPS);
-    delay(5);
-    network.begin(90, node);
-    radio.printDetails();
+JNIEXPORT void JNICALL Java_br_com_diegosilva_rfnative_RfNative_start(JNIEnv *env, jobject thiz, jint node)
+{
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  delay(5);
+  network.begin(90, node);
+  radio.printDetails();
 
-    jclass thisClass = env->GetObjectClass(thiz);
-    jmethodID onReceive = env->GetMethodID(thisClass, "onReceive", "(Ljava/lang/String;)V");
+  jclass thisClass = env->GetObjectClass(thiz);
+  jmethodID onReceive = env->GetMethodID(thisClass, "onReceive", "(Ljava/lang/String;)V");
 
-    while (1)
+  while (1)
+  {
+    network.update();
+    if (network.available())
     {
-        network.update();
-        if (network.available())
-        {
-            char msg[30];
-            RF24NetworkHeader header;
-            network.read(header, &msg, sizeof(msg));
-            char *buf = (char*)malloc(30);
-            strcpy(buf, msg);
-            jstring jstrBuf = env->NewStringUTF(buf);
-            env->CallVoidMethod(thiz, onReceive, jstrBuf);
-        }
+      char msg[30];
+      RF24NetworkHeader header;
+      network.read(header, &msg, sizeof(msg));
+      char *buf = (char *)malloc(30);
+      strcpy(buf, msg);
+      jstring jstrBuf = env->NewStringUTF(buf);
+      env->CallVoidMethod(thiz, onReceive, jstrBuf);
     }
   }
+}
 
-JNIEXPORT jboolean JNICALL Java_br_com_diegosilva_rfnative_RfNative_send
-  (JNIEnv *env, jobject thiz, jint node, jstring jmsg){
-      const char *msg = env->GetStringUTFChars(jmsg, 0);
-      printf("enviando: %s", msg);
-      RF24NetworkHeader header2(node);
-      return network.write(header2, msg, sizeof(msg));
-  }
+JNIEXPORT jboolean JNICALL Java_br_com_diegosilva_rfnative_RfNative_send(JNIEnv *env, jobject thiz, jint node, jstring jmsg)
+{
+  const char *msg = env->GetStringUTFChars(jmsg, 0);
+  RF24NetworkHeader header2(node);
+  return network.write(header2, msg, sizeof(msg));
+}

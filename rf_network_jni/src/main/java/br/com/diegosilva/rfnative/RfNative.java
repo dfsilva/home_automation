645cf12da630d884d6a4a7d7a618bde20b1a8296
Diego <diegosiuniube@gmail.com>
2020-05-19 08:42:39 -0300

    ajustes

--- rf_network_jni/src/main/java/br/com/diegosilva/rfnative/RfNative.java
+++ rf_network_jni/src/main/java/br/com/diegosilva/rfnative/RfNative.java
@@ -1,7 +1,7 @@
 package br.com.diegosilva.rfnative;
 
 public class RfNative {
-    final private static String LIB_NAME = "rfnative";
+    final private static String LIB_NAME = "rfnative.so";
 
     static {
         System.loadLibrary(LIB_NAME);

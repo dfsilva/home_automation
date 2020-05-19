d82ae56e86a919e6ed3e9ef240971f3633c48f19
Diego <diegosiuniube@gmail.com>
2020-05-18 22:23:27 -0300

    sjuates

--- rf_network_jni/src/main/java/MainTest.java
+++ rf_network_jni/src/main/java/MainTest.java
@@ -2,6 +2,7 @@ import br.com.diegosilva.rfnative.RfNative;
 
 public class MainTest {
     public static void main(String ... args){
+        System.out.println(System.getProperty("java.library.path"));
         System.out.println(new RfNative().getString());
     }
 }

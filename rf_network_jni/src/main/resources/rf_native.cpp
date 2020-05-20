62468b41d7a885a816759a19217edb9df52cec66
Diego <diegosiuniube@gmail.com>
2020-05-20 14:19:34 -0300

    f

--- rf_network_jni/src/main/resources/rf_native.cpp
+++ rf_network_jni/src/main/resources/rf_native.cpp
@@ -39,10 +39,12 @@ JNIEXPORT void JNICALL Java_br_com_diegosilva_rfnative_RfNative_start(JNIEnv *en
 
 JNIEXPORT jboolean JNICALL Java_br_com_diegosilva_rfnative_RfNative_send(JNIEnv *env, jobject thiz, jint node, jstring jmsg)
 {
-  const char *msg = env->GetStringUTFChars(jmsg, 0);
+  const char *msgbuf = env->GetStringUTFChars(jmsg, 0);
+  char msg[30];
+  strcpy(msg, msgbuf);
   RF24NetworkHeader header2(node);
   printf("enviando %s ", msg);
-  bool enviou = network.write(header2, msg, sizeof(msg));
-  env->ReleaseStringUTFChars(jmsg, msg);
+  bool enviou = network.write(header2, &msg, sizeof(msg));
+  env->ReleaseStringUTFChars(jmsg, msgbuf);
   return enviou;
 }

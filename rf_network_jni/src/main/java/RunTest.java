df06c6ae147c4cf4b15b6c8d945ee8af3777cd53
Diego <diegosiuniube@gmail.com>
2020-05-20 08:10:54 -0300

    f

--- rf_network_jni/src/main/java/RunTest.java
+++ rf_network_jni/src/main/java/RunTest.java
@@ -4,7 +4,7 @@ public class RunTest {
     public static void main(String ... args){
         new RfNative((instance, msg) -> {
             System.out.println(msg);
-            System.out.println("Enviou: "+instance.send(02, "teste"));
+            System.out.println("Enviou: "+instance.send(02, msg));
         }).start(00);
     }
 }

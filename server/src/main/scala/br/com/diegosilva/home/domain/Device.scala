bc1907d537cac9494332dadc749ea4f6ecdf338d
Diego <diegosiuniube@gmail.com>
2020-05-21 08:10:59 -0300

    fix

---
+++ server/src/main/scala/br/com/diegosilva/home/domain/Device.scala
@@ -0,0 +1,3 @@
+package br.com.diegosilva.home.domain
+
+case class Device(val id : Long)
---
+++ server/src/main/scala/br/com/diegosilva/home/domain/User.scala
@@ -0,0 +1,3 @@
+package br.com.diegosilva.home.domain
+
+case class User(val uid: String, val name: String, val email: String)

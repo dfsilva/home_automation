61095b534d563bffeb121f75c7fe45dc041267f7
Diego Silva <diegosiuniube@gmail.com>
2020-05-21 18:31:33 +0100

    f

--- rf_network_jni/build.sh
+++ rf_network_jni/build.sh
@@ -5,6 +5,6 @@ then
 	exit 1
 else	
 rm src/main/resources/*.so
-g++ -shared -lrf24-bcm -lrf24network -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux src/main/resources/rf_native.cpp -o src/main/resources/librfnative.so
+g++ -shared -lrf24-bcm -lrf24network -lwiringPi -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux src/main/resources/rf_native.cpp -o src/main/resources/librfnative.so
 mvn clean package
 fi
